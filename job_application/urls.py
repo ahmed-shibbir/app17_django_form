38cf7dad5d357b9b8a444300e2d01f0fd4127b6e
shibbir <shibbir63@yahoo.com>
2023-11-15 18:54:31 +0000

    Add navigation bar.

--- job_application/urls.py
+++ job_application/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views   # . refers to current folder. it is possible because urls and views file are in the same directory
 
 urlpatterns = [
-    path('', views.index, name='index') # 3 arguments, # empty string '' relates to home page, otherwise a sub directory  like about etc should hsave been provided.
-
+    path('', views.index, name='index'),# 3 arguments, # empty string '' relates to home page, otherwise a sub directory  like about etc should hsave been provided.
+    path('about/', views.about, name='about')
 ]
